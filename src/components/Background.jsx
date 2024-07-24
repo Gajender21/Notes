a6087da1a523d839dbc17f2dd7b8a7eb1c1ca2e7
Gajender <singhgajender026@gmail.com>
2024-07-24 14:23:47 +0530

    update

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,7 +3,6 @@ import React from "react";
 function Background({ theme }) {
   return (
     <div className="fixed z-[2] w-full h-screen">
-      {" "}
       <h1
         className={
           "absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[10vw] leading-none tracking-tighter font-medium " +
